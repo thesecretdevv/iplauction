2c9748fc5d726582a63a1cb86544ca84665b6471
AnuragTummapudi <tummapudianurag@gmail.com>
2026-04-12 12:04:05 +0530

    added other two methods

--- boom/app/layout.jsx
+++ boom/app/layout.jsx
@@ -57,6 +57,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-2556312199314107" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
